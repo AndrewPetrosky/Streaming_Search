115edbe37cbcf570e8554be1c155235c72fc0fd0
AndrewPetrosky <petroskyandrew@gmail.com>
2016-09-16 11:02:43 -0400

    Implement the argparse functionality with movie flag option.

--- src/search.py
+++ src/search.py
@@ -5,11 +5,16 @@ import argparse
 #@author AndrewPetrosky
 
 def main():
+	parser = argparse.ArgumentParser()
 
-	title = sys.argv[1]
-	webbrowser.open('https://www.netflix.com/search/' + title)
+	parser.add_argument('--movie', action='store_true')
+	parser.add_argument('title')
+
+	args = parser.parse_args()
+
+	webbrowser.open('https://www.netflix.com/search/' + args.title)
 	webbrowser.open('https://www.amazon.com/s/ref=nb_ss_gw/102-1882688-6100927?initialSearch=1&url=search-alias%3Daps&field-keywords='
-		+ title + '&Go.x=0&Go.y=0&Go=Go')
+		+ args.title + '&Go.x=0&Go.y=0&Go=Go')
 	#https://www.hbonow.com/feature/PROD775717/sisters?cid=undefined
 
 if __name__ == "__main__":
