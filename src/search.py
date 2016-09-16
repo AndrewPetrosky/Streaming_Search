9adcb233aa80dabd61661338c9ad60021542e67d
AndrewPetrosky <petroskyandrew@gmail.com>
2016-09-16 15:27:03 -0400

    Add comments to website specific actions

--- src/search.py
+++ src/search.py
@@ -7,15 +7,18 @@ import re
 
 def main():
 	parser = argparse.ArgumentParser()
-
 	parser.add_argument('--movie', action='store_true')
 	parser.add_argument('title')
-
 	args = parser.parse_args()
 
+	#Open the web browser to the Netflix search results
 	webbrowser.open('https://www.netflix.com/search/' + re.sub(r' ', '%20', args.title))
-	webbrowser.open('https://www.amazon.com/s/ref=nb_ss_gw/102-1882688-6100927?initialSearch=1&url=search-alias%3Daps&field-keywords='
-		+ args.title + '&Go.x=0&Go.y=0&Go=Go')
+
+	#Open the web browser to the Amazon search results
+	webbrowser.open('https://www.amazon.com/s/ref=nb_ss_gw/102-1882688-6100927?initialSearch' +
+		'=1&url=search-alias%3Daps&field-keywords=' + args.title + '&Go.x=0&Go.y=0&Go=Go')
+
+	#Open the web browser to the HBO search results
 	if args.movie:
 		webbrowser.open('http://www.hbo.com/movies/' + args.title)
 	else:
