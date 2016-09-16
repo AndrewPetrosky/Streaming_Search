c67a41539a3827827d5976877a3f4ddff56b7e82
AndrewPetrosky <petroskyandrew@gmail.com>
2016-09-16 10:32:02 -0400

    Add link for Amazon.

--- src/search.py
+++ src/search.py
@@ -6,6 +6,9 @@ import sys
 def main():
 	title = sys.argv[1]
 	webbrowser.open('https://www.netflix.com/search/' + title)
+	webbrowser.open('https://www.amazon.com/s/ref=nb_ss_gw/102-1882688-6100927?initialSearch=1&url=search-alias%3Daps&field-keywords='
+		+ title + '&Go.x=0&Go.y=0&Go=Go')
+	#https://www.hbonow.com/feature/PROD775717/sisters?cid=undefined
 
 if __name__ == "__main__":
  	main()
